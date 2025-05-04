52ef6b3c2e863af28a5815c17e39973db3c244e0
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-05-04 15:22:09 +0200

    Helper dto classes created

---
+++ src/Mock_Test_01.Models/CountryDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Mock_Test_01.Models;
+
+public class CountryDto
+{
+    public string CountryName { get; set; }
+}
---
+++ src/Mock_Test_01.Models/CurrencyDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Mock_Test_01.Models;
+
+public class CurrencyDto
+{
+    public string CurrencyName { get; set; }
+    public double Rate { get; set; }
+}
